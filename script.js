04f874c76f8b830244d06cfb056a691a32d0b36f
Júlio Freitas <81449489+devJulio96@users.noreply.github.com>
2024-11-24 01:14:20 -0300

    1ª - Atualização
    - Correção da Issue: "Operações com inteiros resultando em float"
    - Implementação de "alert" após o uso da calculadora.
    - Ao clicar no operador matemático pela segunda vez, com os numeros da operação 
    já definidos a calculadora está permitindo operações consecutivas.

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
-const display = document.querySelector(".display");
-const teclasNum = [...document.querySelectorAll(".num")];
-const teclasOp = [...document.querySelectorAll(".oper")];
-const teclaPonto = document.getElementById("tPonto");
-const teclaApag = document.getElementById("tApag");
-const teclaLimp = document.getElementById("tLimp");
-const teclaRes = document.getElementById("tRes");
+let operador;
 let valores = [0];
 let decimal = false;
-let operador;
+let calculouRes = false;
+
+const teclaRes = document.getElementById("tRes");
+const display = document.querySelector(".display");
+const teclaApag = document.getElementById("tApag");
+const teclaLimp = document.getElementById("tLimp");
+const teclaPonto = document.getElementById("tPonto");
+const teclasOp = [...document.querySelectorAll(".oper")];
+const teclasNum = [...document.querySelectorAll(".num")];
 
 //Calcula o Resultado da operação
 const obterRes = () => {
     let resultado;
+    calculouRes = true;
 
     if(valores[1] != undefined){
         if(display.innerHTML.includes(".")){
@@ -33,40 +36,53 @@ const obterRes = () => {
                 break;
         }
         
-        resultado = Math.fround(resultado).toFixed(2);
-        display.innerHTML = resultado;
-    
-        if(display.innerHTML.includes("Infinity") ||display.innerHTML.includes("NaN")){
+        //Verifica a saída numérica do resultado
+        display.innerHTML = resultado.toLocaleString("en-US");
+        
+        if(display.innerHTML.includes("Infinity") ||display.innerHTML.includes("NaN")|| display.innerHTML.includes("∞")){
             display.innerHTML = "#####Erro#####"
             operador = undefined;
-            valores.pop();
+            valores.length = 0;
         } else{
             operador = undefined;
-            valores.pop();
-            valores[0] = Number(display.innerHTML);
+            valores.length = 0;
         }
     }
+}
+
+//"Zera" todas as variáveis
+const zerarCalc = () => {
+    display.innerHTML ="0";
+    decimal = false;
+    operador = undefined;
+    valores.length = 0;
+    calculouRes = false;
 }
 
 //Insere os valores dos numeros pressionados nos botões
 teclasNum.forEach((el)=>{
     el.addEventListener("click", (evt)=>{
         const btnNum = evt.target.innerHTML;
-        if(display.innerHTML == "0"){
-            display.innerHTML ="";
-            display.innerHTML += btnNum;     
-        } else {
-            display.innerHTML += btnNum;
-        }
         
-        if(valores[0] >= 0){
-            valores = display.innerHTML.split(/\-|\/|\x|\+/).map(Number);
+        if(calculouRes == true){
+            alert("Clique no botão C")
         } else {
-            if(operador == undefined){
-                valores = display.innerHTML.split(/\/|\x|\+/).map(Number);
+            if(display.innerHTML == "0"){
+                display.innerHTML ="";
+                display.innerHTML += btnNum;     
             } else {
-                valores[1] += btnNum;
-                valores[1] = Number(valores[1].replace('undefined',""));
+                display.innerHTML += btnNum;
+            }
+            
+            if(valores[0] >= 0){
+                valores = display.innerHTML.split(/\-|\/|\x|\+/).map(Number);
+            } else {
+                if(operador == undefined){
+                    valores = display.innerHTML.split(/\/|\x|\+/).map(Number);
+                } else {
+                    valores[1] += btnNum;
+                    valores[1] = Number(valores[1].replace('undefined',""));
+                }
             }
         }
     })
@@ -77,50 +93,60 @@ teclasOp.forEach((el)=>{
     el.addEventListener("click", (evt)=>{
         let btnOp = evt.target.innerHTML;
         decimal = false;
-        if(operador != undefined){
-            if(valores[1] == undefined){
-                operador = btnOp;
-                display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length-1) + `${operador}`;
-            } else {
-                obterRes();
-                operador = btnOp;
-                display.innerHTML += operador;
-            } 
+
+        if(calculouRes == true){
+            alert("Clique no botão C")
         } else {
-            operador = btnOp;
-            display.innerHTML += btnOp;
+            if(operador != undefined){
+                if(valores[1] == undefined){
+                    operador = btnOp;
+                    display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length-1) + `${operador}`;
+                } else {
+                    obterRes();
+                    calculouRes = false;
+                    valores[0] = Number(display.innerHTML);
+                    operador = btnOp;
+                    display.innerHTML += operador;
+                } 
+            } else {
+                operador = btnOp;
+                display.innerHTML += btnOp;
+            }
         }
     })
 })
 
 //Insere ponto no display
 teclaPonto.addEventListener("click", () =>{
-    if(!decimal) {
-        decimal = true;
-        display.innerHTML += ".";
+    if(calculouRes == true){
+        alert("Clique no botão C")
+    } else {
+        if(!decimal) {
+            decimal = true;
+            display.innerHTML += ".";
+        }
     }
 })
 
 //Apaga o display e o display e os valores recebidos
-teclaApag.addEventListener("click", () => {
-    display.innerHTML ="0";
-    decimal = false;
-    operador = undefined;
-    valores.length = 0;
-})
+teclaApag.addEventListener("click", zerarCalc)
 
 //Limpa o ultimo numero do display
 teclaLimp.addEventListener("click",()=>{
-    if(display.innerHTML.length == 1 ){
-        display.innerHTML ="0"
+    if(calculouRes == true) {
+        alert("Clique no botão C")
     } else {
-        display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length-1)
+        if(display.innerHTML.length == 1 ){
+            display.innerHTML ="0"
+        } else {
+            display.innerHTML = display.innerHTML.slice(0,display.innerHTML.length-1)
+        }
+    
+        if(valores[1] == "" || valores[1] == undefined){
+            operador = undefined;
+        }
+        valores = display.innerHTML.split(/\-|\/|\x|\+/).map(Number);
     }
-
-    if(valores[1] == "" || valores[1] == undefined){
-        operador = undefined;
-    }
-    valores = display.innerHTML.split(/\-|\/|\x|\+/).map(Number);
 })
 
 //Insere o resultado da operação no display
